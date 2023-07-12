9ee7a9d7e62677805d9846b6fcff7b969f809506
yousaf wazir <umbrellaman612@gmail.com>
2023-07-12 16:27:13 +0100

    added mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@ import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-screen-xl h-[50rem] px-4 xl:px-0 xl:grid xl:grid-flow-col xl:grid-rows-4 xl:gap-6">
-      <div className="xl:row-span-2">
+    <div className="mx-auto max-w-screen-xl xl:h-[50rem] px-4 xl:px-0 xl:grid xl:grid-flow-col xl:grid-rows-4 xl:gap-6">
+      <div className="h-[20rem] xl:h-auto xl:row-span-2">
         <Card className="w-full h-full">hello</Card>
       </div>
-      <div className="xl:row-span-2">
+      <div className="h-[20rem] xl:h-auto xl:row-span-2">
         <Card className="w-full h-full">hello</Card>
       </div>
-      <div className="xl:row-span-4">
+      <div className="h-[40rem] xl:h-auto xl:row-span-4">
         <Card className="w-full h-full">hello</Card>
       </div>
-      <div className="xl:row-span-1">
+      <div className="h-[10rem] xl:h-auto xl:row-span-1">
         <Card className="w-full h-full">hello</Card>
       </div>
-      <div className="xl:row-span-3">
+      <div className="h-[30rem] xl:h-auto xl:row-span-3">
         <Card className="w-full h-full">hello</Card>
       </div>
     </div>
