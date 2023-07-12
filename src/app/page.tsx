dd70020e8dd0839354f3a4c647e565b1b0c79e95
yousaf wazir <umbrellaman612@gmail.com>
2023-07-12 15:20:59 +0100

    added design desktop main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
 export default function Home() {
   return (
-    <div className="px-4">
-      home
+    <div className="mx-auto max-w-screen-xl h-[50rem] border px-4 xl:px-0 grid grid-flow-col grid-rows-4">
+      <div className="row-span-2 border">Left Card 1</div>
+      <div className="row-span-2 border">Left Card 2</div>
+      <div className="row-span-4 border">Middle Card</div>
+      <div className="row-span-1 border">Right Card 1</div>
+      <div className="row-span-3 border">Right Card 2</div>
     </div>
   );
 }
