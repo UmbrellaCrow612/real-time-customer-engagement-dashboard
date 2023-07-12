02688f7027c3ca834b16eb1f4c420e3f648a3ff1
yousaf wazir <umbrellaman612@gmail.com>
2023-07-12 15:29:22 +0100

    added card skeletons to desktop v

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import { Card } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <div className="mx-auto max-w-screen-xl h-[50rem] border px-4 xl:px-0 grid grid-flow-col grid-rows-4">
-      <div className="row-span-2 border">Left Card 1</div>
-      <div className="row-span-2 border">Left Card 2</div>
-      <div className="row-span-4 border">Middle Card</div>
-      <div className="row-span-1 border">Right Card 1</div>
-      <div className="row-span-3 border">Right Card 2</div>
+    <div className="mx-auto max-w-screen-xl h-[50rem] px-4 xl:px-0 xl:grid xl:grid-flow-col xl:grid-rows-4 xl:gap-6">
+      <div className="xl:row-span-2">
+        <Card className="w-full h-full">hello</Card>
+      </div>
+      <div className="xl:row-span-2">
+        <Card className="w-full h-full">hello</Card>
+      </div>
+      <div className="xl:row-span-4">
+        <Card className="w-full h-full">hello</Card>
+      </div>
+      <div className="xl:row-span-1">
+        <Card className="w-full h-full">hello</Card>
+      </div>
+      <div className="xl:row-span-3">
+        <Card className="w-full h-full">hello</Card>
+      </div>
     </div>
   );
 }
